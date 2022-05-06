4c1a3ee088ff869cfcb7beec54134d502dfeb650
fan-yang-sonarsource <fan.yang@sonarsource.com>
2022-05-06 11:07:16 +0200

    first commit

---
+++ src/main/java/com/acme/basic/HelloWorld.java
@@ -0,0 +1,36 @@
+package com.acme.basic;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HelloWorld {
+
+  void sayHello() {
+    System.out.println("Hello World!");
+  }
+
+  String getName(String a){
+    // if(a.equal("hello")) {
+    //   return "name";
+    // } else {
+      return null;
+    // }
+  }
+
+  void notCovered() {
+    System.out.println("This method is not covered by unit tests");
+  }
+
+  boolean isNameEmpty() {
+    return getName("world").length() == 0; // Noncompliant; the result of getName() could be null, but isn't null-checked
+  }
+
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    String name = req.getParameter("name");
+    PrintWriter out = resp.getWriter();
+    out.write("Hello " + name); // Noncompliant
+  }
+
+}
