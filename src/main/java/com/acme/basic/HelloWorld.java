8dbcd917eed40019bd4609fbca5283f52720c7e2
fan.yang <fan.yang@sonarsource.com>
2025-03-04 15:59:12 +0100

    test

--- src/main/java/com/acme/basic/HelloWorld.java
+++ src/main/java/com/acme/basic/HelloWorld.java
@@ -35,10 +35,6 @@ public class HelloWorld {
     System.out.println("This method is not covered by unit tests");
   }
 
-  void notCovered5() {
-    System.out.println("This method is not covered by unit tests");
-  }
-
   boolean isNameEmpty() {
     return getName("world").length() == 0; // Noncompliant; the result of getName() could be null, but isn't null-checked
   }
